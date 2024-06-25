908b4354e53955d5544893ccacdc3ebb1f2abbdd
nadimmostafa <nadimmostafa334@gmail.com>
2024-06-25 18:13:11 +0600

    add user api and show in ui

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-const express = require('express')
-const app = express()
-const cors = require("cors")
+const express = require("express");
+const app = express();
+const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT || 5000;
 
-
-
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rrkijcq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,57 +18,87 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
-    const CollectionFMenu = client.db("BistroBossDB").collection('OrderDB')
-    const CollectionFReview = client.db("BistroBossReviewDB").collection('ReviewDB')
-    const CollectionFCarts = client.db("BistroBossReviewDB").collection('CartsDB')
+  const CollectionFMenu = client.db("BistroBossDB").collection("OrderDB");
+  const CollectionFReview = client
+    .db("BistroBossReviewDB")
+    .collection("ReviewDB");
+  const CollectionFCarts = client
+    .db("BistroBossReviewDB")
+    .collection("CartsDB");
+  const CollectionFUsers = client
+    .db("BistroBossReviewDB")
+    .collection("UsersDB");
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-    app.get('/menu', async(req, res) => {
-        const menu = req.body
-        const result = await CollectionFMenu.find(menu).toArray()
-        res.send(result)
-      })
+    app.get("/menu", async (req, res) => {
+      const menu = req.body;
+      const result = await CollectionFMenu.find(menu).toArray();
+      res.send(result);
+    });
 
+    app.get("/review", async (req, res) => {
+      const review = req.body;
+      const result = await CollectionFReview.find(review).toArray();
+      res.send(result);
+    });
 
-    app.get('/review', async(req, res) => {
-        const review = req.body
-        const result = await CollectionFReview.find(review).toArray()
-        res.send(result)
-      })
+    // cart related api
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const filter = { email: email };
+      const result = await CollectionFCarts.find(filter).toArray();
+      res.send(result);
+    });
 
-      // cart related api
-      app.get('/carts', async(req,res)=>{
-        const email =  req.query.email
-        const filter = {email: email}
-        const result = await CollectionFCarts.find(filter).toArray()
-        res.send(result)
-      })
+    app.post("/carts", async (req, res) => {
+      const cart = req.body;
+      const result = await CollectionFCarts.insertOne(cart);
+      res.send(result);
+    });
 
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const result = await CollectionFCarts.deleteOne(filter);
+      res.send(result);
+    });
 
-      app.post('/carts', async(req,res)=>{
-        const cart = req.body;
-        const result = await CollectionFCarts.insertOne(cart)
-        res.send(result)
+    // user related api
+    app.post("/user", async (req, res) => {
+      const user = req.body;
+      const filter = { email: user.email };
+      const exiting = await CollectionFUsers.findOne(filter);
+      if (exiting) {
+        return res.send({ message: "user already exit" });
+      }
 
-      })
+      const result = await CollectionFUsers.insertOne(user);
+      res.send(result);
+    });
 
-      app.delete('/carts/:id', async(req,res)=>{
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)}
-        const result = await CollectionFCarts.deleteOne(filter)
-        res.send(result)
-      })
+    app.get('/user', async(req,res)=>{
+      const user = await CollectionFUsers.find().toArray()
+      res.send(user)
+    })
 
+    app.delete('/user/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const result = await CollectionFUsers.deleteOne(filter)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -79,12 +106,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("server is connection...!");
+});
 
-
-app.get('/', (req, res) => {
-    res.send('server is connection...!')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Example app listening on port, ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port, ${port}`);
+});
