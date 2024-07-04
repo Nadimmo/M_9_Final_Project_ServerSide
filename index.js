8a4f21e7917a6464c90952e5b1dc535e42709f99
nadimmostafa <nadimmostafa334@gmail.com>
2024-07-04 10:55:04 +0600

    post menu in database and show in ui

--- index.js
+++ index.js
@@ -86,6 +86,19 @@ async function run() {
       res.send(result);
     });
 
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await CollectionFMenu.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+      const item = req.body;
+      const result = await CollectionFMenu.insertOne(item);
+      res.send(result);
+    });
+
     app.get("/review", async (req, res) => {
       const review = req.body;
       const result = await CollectionFReview.find(review).toArray();
